4014fd59a3cb80f89c2960216bfde110cf622833
Evgeny <djek.uait1998@gmail.com>
2023-01-24 18:05:15 +0300

    adaptiv height slider

--- src/blocks/slider/slider.js
+++ src/blocks/slider/slider.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
     $(".slider__main").slick({
         prevArrow:".slider__arrow--prev",
-        nextArrow:".slider__arrow--next"
+        nextArrow:".slider__arrow--next",
+        adaptiveHeight: true
 
     })
 })
